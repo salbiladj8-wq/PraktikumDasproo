eaa817ac824f95574eb6aeed9a5bc838cdb4ea3a
Salbila <salbiladj8@gmail.com>
2025-09-30 19:21:54 -0700

    Percobaan1

---
+++ jobsheet5/ifCetakKRS28.java
@@ -0,0 +1,20 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ifCetakKRS28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS SIAKAD ----");
+        System.out.println("Apakah UKT sudah Lunas? (true/false):");
+        boolean uktLunas = sc.nextBoolean();
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan");
+        } else {
+            System.out.println("Registrasi ditolak, silahkan lunasi UKT terlebih dahulu");
+        }
+    }
+}
+
