28ff8488d6ae537049aae3076eae454b291cecf2
Salbila <salbiladj8@gmail.com>
2025-09-30 21:06:19 -0700

    Modifikasi Percobaan1

--- jobsheet5/ifCetakKRS28.java
+++ jobsheet5/ifCetakKRS28.java
@@ -11,7 +11,7 @@ public class ifCetakKRS28 {
         boolean uktLunas = sc.nextBoolean();
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
         } else {
             System.out.println("Registrasi ditolak, silahkan lunasi UKT terlebih dahulu");
         }
